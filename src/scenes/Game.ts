afe427a315156fb093b601da39a528ee95e8fff5
Colin Kiama <colinkiama@gmail.com>
2022-12-08 15:00:02 +0000

    Add bouncing ball sprite

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,18 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export default class Demo extends Phaser.Scene {
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image("ball", "assets/default-ball.png");
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+    const ball = this.physics.add.image(100, 450, "ball");
+    ball.setBounce(1);
+    ball.setCollideWorldBounds(true);
+    ball.body.setGravityY(1000);
   }
 }
